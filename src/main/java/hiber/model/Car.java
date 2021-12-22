22d432f6cec64d0bb5388331d68fd0afc462beb2
DanyaRubin <masterovski@gmail.com>
2021-12-22 22:37:19 +0400

    1

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -24,5 +24,36 @@ public class Car {
         this.idCar = idCar;
     }
 
+    public Car() {
+    }
 
+    public Car(Long idCar, String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "idCar=" + idCar +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
 }
