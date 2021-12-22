e1a60898a6a01c5ac6d6d97f8d19f3393fe590cc
DanyaRubin <masterovski@gmail.com>
2021-12-22 16:10:46 +0400

    Initial commit

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,15 @@
+package hiber.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Table(name = "cars")
+public class Car {
+
+    private String model;
+    private int series;
+
+
+
+}
